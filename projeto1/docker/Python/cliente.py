8058fdcff9b2d9684aae5ea7da9530c6f3f09254
RafoxQueen <rafox.queen@gmail.com>
2023-06-18 17:10:45 -0300

    Teste de conexão TCP

--- projeto1/docker/Python/cliente.py
+++ projeto1/docker/Python/cliente.py
@@ -1,1 +1,11 @@
-print("Cliente")
+import socket
+
+s = socket.socket()
+print("Socket succefully created")
+port = 9000
+
+s.connect(("10.9.0.10", port))
+
+print(s.recv(1024).decode())
+
+s.close()
--- projeto1/docker/Python/server.py
+++ projeto1/docker/Python/server.py
@@ -1,2 +1,20 @@
 #Servidor
-print("Funciona")
+import socket
+
+s = socket.socket()
+print("Socket succefully created")
+port = 9000
+
+s.bind(("", port))
+print("socket binded to %s"%(port))
+
+s.listen(5)
+print("Socket is listening")
+
+while True:
+	c, addr = s.accept()
+	print("Got connection from ", addr)
+	c.send("Thank you for connectig".encode())
+	c.close()
+	
+	break
