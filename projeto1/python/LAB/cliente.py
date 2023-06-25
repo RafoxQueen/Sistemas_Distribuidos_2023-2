ec9ba53596f3f81dd5c065bff8320994a91c65ef
Rafox <rafox.queen@gmail.com>
2023-06-25 19:57:33 -0300

    Modificacoes nos arquivos principais e adicao de comentarios

--- projeto1/python/LAB/cliente.py
+++ projeto1/python/LAB/cliente.py
@@ -1,90 +1,131 @@
+# -*- coding: utf-8 -*-
+#@author: Rafox
+
+#Projeto de Programação – Napster com Sockets em Python
+
+#Cliente
+
+#Importacao de bibliotecas
 import socket, threading, os
 
 print("Olá, seja bem vindo!!")
 
+#definicao de constante para mantes o sistema e threads funcionando
 serverOn = True
+
 
 def main():
     while True:
+        #Criacao do menu do console
         operacao = int(input("\nDigite a operação que deseja fazer:\n1 - JOIN\n2 - SEARCH\n3 - DOWNLOAD\nEscolha> "))
         
+        #Opcao para fazer JOIN no servidor
         if operacao == 1:
+            #Criacao do socket
             server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             try:
-            
-                #ip = input("Por favor, digite o IP do servidor para conexão> ")
-                #port = int(input("Insira o número da porta> "))
-            
-                portaServ = int(input("\nDigite a porta de conexão a esta máquina> "))
-                print("Se estiver no windows use \\ para representar os diretórios, para uma pasta anterior a desse arquivo (../) ou (../Pasta_desejada)")
-                print("Não se esqueça de colocar \\ ou / apos o nome do último diretório")
+                #Definicao de variaveis importantes para a conecao
+                #IP do servidor
+                ip = input("/nPor favor, digite o IP do servidor para conexão> ")
+                #Porta do servidor
+                port = int(input("Insira o número da porta(a padrão é 1099)> "))
+                #Porta de esculta do cliente para outros Peers
+                portaServ = int(input("Digite a porta de conexão a esta máquina> "))
+                print("Se estiver no windows use \\\ para representar os diretórios, para uma pasta anterior a desse arquivo (../) ou (../Pasta_desejada)")
+                print("Não se esqueça de colocar \\\ ou / apos o nome do último diretório")
+                #Diretorio onde sera verificado a presensa de arquivos e para Download
                 diretorioD = input("Digite o diretório para armazenar arquivos compartilhaveis> ")
                 
-                ip = "127.0.0.1"
-                port = 1099
-                #portaServ = 9123
-                #diretorioD = "B:\\test"
-                
+                #Conecta-se ao servidor
                 server.connect((ip, port))
                 
+                #Preparacao da requisicao para o servidor
                 msg = ""
+                #Informa a requisicao JOIN e a porta que o Peer recebe requisicao
                 ip_conec = f"JOIN;{portaServ}:"
-                
+                #Lista arquivos no diretorio informado
                 arquivos = os.listdir(diretorioD)
                 
+                #Formata arquivos para envio na requisicao
                 for arquivo in arquivos:
                     msg += f"{arquivo},"
                 
                 arquivos = msg
-                
+                #Adiciona a informacao de que foi enviado a mensagem completa
                 msg = ip_conec + msg + ";fim"
+                
+                #Envia mensagem
                 server.send(msg.encode())
-
+                
+                #Espera resposta do servidor
                 res = server.recv(1024).decode()
+                
+                #Se a conexao foi bem sucedida informa ao usuario
                 if res == "JOIN_OK":
                     ip_local = server.getsockname()[0]
                     print(f"\nSou peer {ip_local}:{portaServ} com arquivos {arquivos}")
                 
+                #Inicia thread responsavel pelo servidor do Peer
                 server_thread = threading.Thread(target=servDownload, args=[portaServ,diretorioD])
-                stop_event = threading.Event()
                 server_thread.start()
                 
-                
             except:
+                #Informa se houve algum erro no processo para fazer o JOIN
                 print("Erro no JOIN")
-                
+        
+        #Condicao para fazer requisicao SEARCH
         elif operacao == 2:
+            #Pede o nome do arquivo
             arq = input("\nInsira o nome do arquivo com a extenção do mesmo> ")
+            #Formatacao e envio da requisicao
             msg = f"SEARCH;{arq};fim"
             server.send(msg.encode())
+            #Espera da resposta do servidor
             res = server.recv(1024).decode()
-            print(f"\npeers com arquivo solicitado: {res}")
-            
+            #Informa ao usuario quais IP:Porta tem o arquivo
+            print(f"\nPeers com arquivo solicitado: {res}")
+        
+        #Condicao para fazer a requisicao de DOWNLOAD
         elif operacao == 3:
+            #Cria socket para se comunicar com outro peer
             peer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             
+            #Variaveis contendo dados do peer a ser conectado
             ip = input("Por favor, digite o IP do Peer para conexão> ")
             portaPeer = int(input("\nDigite a porta de conexão do Peer> "))
             
+            #Faz conexao com outro peer
             peer.connect((ip, portaPeer))
+            #Formata e envio da requisicao de pedido com o arquivo informado ao fazer a requisicao SEARCH
             msg = f"DOWNLOAD;{arq};fim"
             peer.send(msg.encode())
             
+            #Prepara para o download do arquivo
             with open(diretorioD+arq, 'wb') as file:
+                #Loop para receber os bytes do arquivo
                 while True:
+                    #Recebe pacotes do servidor
                     data = peer.recv(1000000)
+                    #Condicao se não houver mais bytes
                     if not data:
+                        #Informa que o arquivo foi baixado
+                        print(f"Arquivo {arq} baixado com sucesso na pasta diretorioD")
+                        
+                        #Formataco e envio da requisicao UPDATE ao servidor
                         msg = f"UPDATE;{arq};{portaServ};fim"
                         server.send(msg.encode())
+                        #Espera a resposta do servidor
                         res = server.recv(1024).decode()
+                        #Se nao receber um UPDATE_OK reenvia a mensagem para o servidor
                         while res != "UPDATE_OK":
                             server.send(msg.encode())
                             res = server.recv(1024).decode()
                         
                         break
+                    #Escreve o que foi recebido no arquivo local
                     file.write(data)
             
-            
+        #Opcao oculta para desligar o Peer
         elif operacao == 4:
             print("\nDesligando programa")
             server.close()
@@ -92,41 +133,51 @@ def main():
             serverOn = False
             break
         
+        #Condicao para informar ao usuario que foi digitado algo invalido
         else:
             print("Por favor, digite algo válido")
         
     
-
+#Funcao responsavel pela creacao do servidor de UPLOAD no Peer
 def servDownload(porta, diretorio):
+    #Tenta criar servidor
     try:
+        #Definicao do socket
         servUp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        port = porta
         
-        servUp.bind(("", port))
+        #Inicia esculta do servidor
+        servUp.bind(("", porta))
         servUp.listen(5)
         
+        #Loop para aceitar as requisicoes
+        while serverOn:
+            c, addr = servUp.accept()
+            #Recebimento e tratamento da mensagem recebida
+            msg = c.recv(1024).decode()
+            msg = msg.split(";")
+            #Se for requisicao de DOWNLOAD prepara para o envio
+            if msg[0] == "DOWNLOAD":
+                arquivo = msg[1]
+                #Criacao de thread para o envio do arquivo solicitado
+                upload_thread = threading.Thread(target=uploadServ, args=[c, addr, arquivo,diretorio])
+                upload_thread.start()
+
+    #Informa se ouve problema na criacao do servidor
     except:
         print("\n Não foi possivel criar servidor")
         
-    while serverOn:
-        c, addr = servUp.accept()
-        
-        msg = c.recv(1024).decode()
-        msg = msg.split(";")
-        if msg[0] == "DOWNLOAD":
-            arquivo = msg[1]
-            upload_thread = threading.Thread(target=uploadServ, args=[c, addr, arquivo,diretorio])
-            upload_thread.start()
-            
-    servUp.close()
-        
+#Funcao responsavel por enviar arquivos solicitados
 def uploadServ(c,addr,arquivo, diretorio):
+    #Lista os arquivos do direorio do Peer
     arquivos = os.listdir(diretorio)
+    #Verifica se o arquivo existe no Peer
     if arquivo in arquivos:
+        #Leitura e envio das informacoes presentes no arquivo solicitado
         with open(diretorio+arquivo, "rb") as uploadArquivo:
             for data in uploadArquivo.readlines():
                 c.send(data)
                 
             c.close()
-                
+
+#Chamada de funcao main iniciando o programa
 main()
--- projeto1/python/LAB/server.py
+++ projeto1/python/LAB/server.py
@@ -1,41 +1,61 @@
+# -*- coding: utf-8 -*-
+#@author: Rafox
+
+#Projeto de Programação – Napster com Sockets em Python
+
 #Servidor
+
+#Importação de bibliotecas
 import socket, threading
 
-diretorio = "../teste/"
-with open(diretorio+"test.txt", "w") as arquivo:
+#Definição do diretório onde ficara o arquivo com os registros dos Peers
+entrada = input("Digite o local do diretório do registro do servidor> ")
+diretorio = entrada+"servidor.txt"
+
+#Criacao/limpesa do arquivo de registro
+with open(diretorio, "w") as arquivo:
     text = arquivo.write("")
 
+#Funcao principal para criacao do servidor
 def main():
     try:
+        #Criacao do socket
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         port = 1099
         
+        #Iniciando a esculta do servidor
         s.bind(("", port))
         s.listen(5)
         
     except:
+        #Mensagem de erro
         print("\n Não foi possivel criar servidor")
         
     while True:
+        #Aceita conexão vinda de um Peer
         c, addr = s.accept()
         
-        #aceitarConexao(c, addr)
-        
+        #Criar e iniciar thread para a conexao
         aceita_thread = threading.Thread(target=aceitarConexao, args=[c, addr])
         aceita_thread.start()
 
-
+#Funcao para armazenar os dados dos Peers
 def gravaDados(arqvs,addr,pasta=diretorio):
-    data = ""
+    #Tratamento da entrada dos dados do Peer
     arqvs = arqvs.split(":")
     porta = arqvs[0]
     data = arqvs[1]
     
+    #Print no servifor informando o JOIN do Peer
     print(f"Peer {addr[0]}:{addr[1]} adicionado com arquivos {data}")
+    
+    #Formatação dos dados do Peer
     conec = f"{addr[0]}:{porta}"
     data = conec +";"+data
     data += "\n"
-    with open(pasta+"test.txt", "a") as arquivo:
+    
+    #Gravação dos dados no arquivo do servidor
+    with open(pasta, "a") as arquivo:
         text = arquivo.write(f"{data}")
 
 
@@ -43,81 +63,116 @@ def pesquisaDados(arq, pasta=diretorio):
     arquivos = {}
     chaves = []
     
+    #Leitura do arquivo de registro do servidor
+    with open(pasta, "r") as arquivoS:
+        linhas = arquivoS.readlines()
     
-    with open(pasta+"test.txt", "r") as arquivo:
-        linhas = arquivo.readlines()
-    
+    #Formatação em forma de dicionário para evitar duplicidade nos dados
     for linha in linhas:
         try:
             data = linha.split(";")
             arquivos[data[0]] = data[1].split(",")
         except:
             arquivos["erro"] = linha
-
-    arq=""+arq
-
+    
+    #Busca pelo nome do arquivo pedido
     for chave, valor in arquivos.items():
         if arq in valor:
             chaves.append(chave)
     
     return str(chaves)
 
+#Funcao para realizar as atualizações nos dados
 def atualizaDados(arq, addr, porta,pasta=diretorio):
-    with open(pasta+"test.txt", "r") as arquivo:
+    #Leitura do arquivo do servidor
+    with open(pasta, "r") as arquivo:
         linhas = arquivo.readlines()
     
+    #Busca da linha corre ao Peer que solicitou atualizacao
     for linha in linhas:
         if f"{addr[0]}:{porta}" in linha:
             mod = (linhas.index(linha))
     
+    #Pega linha correspondente ao Peer e formata para modificacao
     modLinha = linhas[mod]
     separa = modLinha.split(";")
-
     separa[1] = separa[1].split(",")
-    if not(arq in separa[1]):
-        separa[1][-1] = arq
-        arq = ""
     
+    #Busca se o arquivo a ser adicionado existe no Peer
+    if not(arq in separa[1]):
+        #Se não existe adiciona na ultima posição da lista, geralmente corresponde a \n vem de arquivo
+        separa[1][-1] = arq
+        
+        #Esvasia a variavel para ser reutilizada
+        arq = ""
+        
+        #Formatacao dos arquivos do Peer
         for partes in separa[1]:
             arq += partes+','
         arq +="\n"
+        
+        #Formatacao da configuracao do servidor do Peer com os arquivos
         novaLinha = f"{separa[0]};{arq}"
         linhas[mod] = novaLinha
             
-        with open(pasta+"test.txt", "w") as arquivo:
+        #Reescreve o arquivo de registro do servidor com a modificacao
+        with open(pasta, "w") as arquivo:
             linha = arquivo.writelines(linhas)
         
-
+#Funcao responsavel por tratar as requisicoes do servidor
 def aceitarConexao(c, addr):
     while True:
         try:
+            #Espera requisicao do Peer
             msg = c.recv(2048).decode()
+            #Trata e formata a requisicao entregue
             msg = msg.split(";")
+            
+            #Condicao caso a requisicao inclua JOIN
             if msg[0] == "JOIN":
+                #Verifica se a mesnagem está completa
                 if msg[-1] == "fim":
+                    #Definicao da variavel com os arquivos enviados
                     arquivos = msg[1]
+                    #Chamada da funcao para fazer o registro no servidor
                     gravaDados(arquivos,addr)
+                    #Envia confirmacao de que ouve o JOIN
                     c.send("JOIN_OK".encode())
                 
+            #Condicao caso a requisicao seja um SEARCH
             elif msg[0] == "SEARCH":
-                arquivo = msg[1]
+                #Verifica se a mesnagem está completa
                 if msg[-1]=="fim":
+                    #Definicao da variavel com o arquivo solicitado para pesquisa
+                    arquivo = msg[1]
+                    #Print no servidor sobre a solicitacao
                     print(f"Peer {addr[0]}:{addr[1]} solicitou arquivo {arquivo}")
+                    
+                    #Variavel que recebe de retorno da funcao pesquisaDados uma lista de IP:Porta
+                    #que tem o arquivo socilitado
                     ips = pesquisaDados(arquivo)
+                    
+                    #Envio da lista
                     c.send(ips.encode())
 
-                
+            #Condicao caso a requisicao seja um UPDATE
             elif msg[0] == "UPDATE":
-                arquivo = msg[1]
+                #Verifica se a mesnagem está completa
                 if msg[-1]=="fim":
+                    #Formatacao das variaveis
+                    arquivo = msg[1]
                     porta = msg[2]
+                    #Chamada da funcao para fazer atualizacao no registro do servidor
                     atualizaDados(arquivo,addr,porta)
+                    #Envio da confirmacao de que ocorreu o UPDATE
                     c.send("UPDATE_OK".encode())
                 
             else:
+                #Caso a requisicao venha errado envia erro para o Peer
                 c.send("Reenvie a mensagem".encode())
         except:
+            #Caso ocorra um problema na conexao entre o Peer e o Servidor, encerra o while e a thread
             break
 
-
+#Chamada da funcao main
 main()
